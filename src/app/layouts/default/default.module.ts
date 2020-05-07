12d2c7ec4e6aa746ea3df92c9dfec83612d7daa4
Hector Garcia <rotceh.1203@gmail.com>
2020-05-07 14:11:36 -0700

    Proyecto

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from '../../modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { DashboardService } from 'src/app/modules/dashboard.service';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule    
+  ],
+  providers: [
+    DashboardService
+  ]
+})
+export class DefaultModule { }
